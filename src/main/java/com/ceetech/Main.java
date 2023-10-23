2a3e4b0a01584abb1f811a765a3aac650b6dd57e
MusiliC <musilibrian07@gmail.com>
2023-10-23 21:16:53 +0300

    custom formatter

--- src/main/java/com/ceetech/Main.java
+++ src/main/java/com/ceetech/Main.java
@@ -1,7 +1,31 @@
 package com.ceetech;
 
+import com.ceetech.util.CustomFormatter;
+
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+
 public class Main {
-    public static void main(String[] args) {
+    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+
+
+    public static void main(String[] args) throws IOException {
+        initializeLogger();
         System.out.println("Hello world!");
+    }
+
+    static public void initializeLogger() {
+        FileHandler handler;
+        try {
+            handler = new FileHandler("log.txt", true);
+            CustomFormatter formatter = new CustomFormatter();
+            LOGGER.addHandler(handler);
+            handler.setFormatter(formatter);
+            LOGGER.info("Starting Application!");
+        } catch (IOException e) {
+            LOGGER.severe(e.getMessage());
+        }
+        ;
     }
 }
